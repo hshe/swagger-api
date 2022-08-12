6dda24585568d72b0c3c6a6eac148894bf79b03c
hshe <hshe@163.com>
2022-08-12 20:55:01 +0800

    feat: update grpc-ecosystem/grpc-gateway version

--- openapiv2/handler.go
+++ openapiv2/handler.go
@@ -2,10 +2,10 @@ package openapiv2
 
 import (
 	"encoding/json"
+	"github.com/go-kratos/swagger-api/openapiv2/generator"
 	"net/http"
 	"net/url"
 
-	"github.com/go-kratos/grpc-gateway/v2/protoc-gen-openapiv2/generator"
 	"github.com/go-kratos/kratos/v2/api/metadata"
 	"github.com/go-kratos/kratos/v2/transport/http/binding"
 	_ "github.com/go-kratos/swagger-api/openapiv2/swagger_ui/statik" // import statik static files
